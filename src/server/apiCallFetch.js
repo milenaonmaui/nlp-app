8bbe704ec84be8bd54f9545540be8993379356cd
Milena Kari <milenakari@yahoo.com>
2021-01-27 13:04:28 -1000

    Get API call to work using fetch

---
+++ src/server/apiCallFetch.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch')
+
+const getSentimentAnalysis = async(url='', key) => {
+    console.log('In getData')
+    const fullURL = 'https://api.meaningcloud.com/sentiment-2.1?key=' + key +'&lang=en&url=' + url;
+    const res = await fetch(fullURL, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+        }
+    });
+    try {
+        const data = await res.json();
+        return {
+            agreement: data.agreement,
+            subjectivity: data.subjectivity,
+            confidence: data.confidence,
+            irony: data.irony, 
+            score_tag: data.score_tag
+        }
+
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+module.exports = getSentimentAnalysis
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
+//API call
+//https://api.meaningcloud.com/sentiment-2.1?key=<API_KEY>&of=json&txt=Main%20dishes%20were%20quite%20good%2C%20but%20desserts%20were%20too%20sweet%20for%20me.&lang=en
+const url = "https://www.cnn.com/politics/live-news/biden-presidency-trump-impeachment-1-27-21/index.html"
+const key = process.env.API_KEY;
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+c//onst mockAPIResponse = require('./mockAPI.js')
+//const apiResponse = require('./apiCall.js');
+const getSentimentAnalysis = require('./apiCallFetch.js');
 
 const app = express()
 
@@ -15,9 +23,10 @@ app.get('/', function (req, res) {
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('NLP app listening on port 8080!')
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    getSentimentAnalysis(url, key)
+    .then(resp => res.send(resp))
 })
